b0a7bff60954c9bc524c8e579f02806536d959a3
Laurynas Butkus <laurynas.butkus@gmail.com>
2011-05-08 14:29:57 +0300

    Add MySQL views support

--- lib/redhillonrails_core/active_record/base.rb
+++ lib/redhillonrails_core/active_record/base.rb
@@ -26,7 +26,7 @@ module RedhillonrailsCore::ActiveRecord
       end
 
       def pluralized_table_name(table_name)
-        ActiveRecord::Base.pluralize_table_names ? table_name.to_s.pluralize : table_name
+        ::ActiveRecord::Base.pluralize_table_names ? table_name.to_s.pluralize : table_name
       end
 
       def indexes
