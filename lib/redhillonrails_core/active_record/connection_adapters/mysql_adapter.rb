a87b8468b8615a2ca9fc21a12591d6b42f001389
Michał Łomnicki <michal.lomnicki@gmail.com>
2011-07-19 20:55:09 +0200

    fixed quoting table name for add/remove_foreign call. tests added

--- lib/redhillonrails_core/active_record/connection_adapters/mysql_adapter.rb
+++ lib/redhillonrails_core/active_record/connection_adapters/mysql_adapter.rb
@@ -17,7 +17,7 @@ module RedhillonrailsCore::ActiveRecord::ConnectionAdapters
     end
 
     def remove_foreign_key(table_name, foreign_key_name)
-      execute "ALTER TABLE #{table_name} DROP FOREIGN KEY #{foreign_key_name}"
+      execute "ALTER TABLE #{quote_table_name(table_name)} DROP FOREIGN KEY #{foreign_key_name}"
     end
 
     def remove_column_with_redhillonrails_core(table_name, *column_names)
@@ -59,64 +59,19 @@ module RedhillonrailsCore::ActiveRecord::ConnectionAdapters
       foreign_keys
     end
 
-#    def reverse_foreign_keys(table_name, name = nil)
-#      results = execute(<<-SQL, name)
-#      SELECT constraint_name, table_name, column_name, referenced_table_name, referenced_column_name
-#        FROM information_schema.key_column_usage
-#        WHERE table_schema = SCHEMA()
-#          AND referenced_table_schema = table_schema
-#        ORDER BY constraint_name, ordinal_position;
-#      SQL
-#      current_foreign_key = nil
-#      foreign_keys = []
-#
-#      results.each do |row|
-#        next unless table_name.casecmp(row[3]) == 0
-#        if current_foreign_key != row[0]
-#          foreign_keys << ForeignKeyDefinition.new(row[0], row[1], [], row[3], [])
-#          current_foreign_key = row[0]
-#        end
-#
-#        foreign_keys.last.column_names << row[2]
-#        foreign_keys.last.references_column_names << row[4]
-#      end
-#
-#      foreign_keys
-#    end
-
     def reverse_foreign_keys(table_name, name = nil)
-#      @@schema ||= nil
-#      @@schema_version ||= 0
-#      current_version = ActiveRecord::Migrator.current_version
-#      if @@schema.nil? || @@schema_version != current_version
-#        @@schema_version = current_version
-#        ans = execute(<<-SQL, name)
-#        SELECT constraint_name, table_name, column_name, referenced_table_name, referenced_column_name
-#          FROM information_schema.key_column_usage
-#         WHERE table_schema = SCHEMA()
-#           AND referenced_table_schema = table_schema
-#         ORDER BY constraint_name, ordinal_position;
-#        SQL
-#        @@schema = []
-#        ans.each do | row |
-#          @@schema << [row[0], row[1], row[2], row[3], row[4]]
-#        end
-#      end
-#      results = @@schema
-
       results = execute(<<-SQL, name)
       SELECT constraint_name, table_name, column_name, referenced_table_name, referenced_column_name
-        FROM information_schema.key_column_usage
-        WHERE table_schema = SCHEMA()
-          AND referenced_table_schema = table_schema
-        ORDER BY constraint_name, ordinal_position;
+      FROM information_schema.key_column_usage
+      WHERE table_schema = SCHEMA()
+      AND referenced_table_schema = table_schema
+      ORDER BY constraint_name, ordinal_position;
       SQL
-
       current_foreign_key = nil
       foreign_keys = []
 
       results.each do |row|
-        next if row[3] != table_name
+        next unless table_name.casecmp(row[3]) == 0
         if current_foreign_key != row[0]
           foreign_keys << ForeignKeyDefinition.new(row[0], row[1], [], row[3], [])
           current_foreign_key = row[0]
